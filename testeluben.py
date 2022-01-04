31aefca38636e517262b3725e6a7d1e96b74415f
luben <luan.silvapvh@gmail.com>
2022-01-04 11:18:51 -0400

    adicionando arquivo lubenteste.py

---
+++ testeluben.py
@@ -0,0 +1,8 @@
+contador = 0
+nome = 'luan'
+while (contador <= 5):
+       print(contador)
+       contador   = contador + 1
+       if contador == 4:
+              nome = str(input('Digite seu nome'))
+print(nome)
