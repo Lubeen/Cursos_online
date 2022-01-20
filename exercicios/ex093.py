8427d4ed9c0781bd829516e5fb7e6af1d30480b2
luben <luan.silvapvh@gmail.com>
2022-01-19 23:01:19 -0400

    atualizando

--- exercicios/ex093.py
+++ exercicios/ex093.py
@@ -3,15 +3,23 @@
   Depois vai ler a quantidade de gols feitos em cada partida.
    No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.'''
 jogador = {}
-gol = gols = 0
+gol = total = 0
 lista = []
-jogador['nome'] = input('nome: ')
-jogador['partidas'] = int(input('Quantas partidas? '))
-for c in range(0, jogador['partidas']):
-    gol = int(input(f'Quantos gols foram marcados na partida {c+1}: '))
+jogador['nome'] = input('nome do jogador: ')
+partidas = int(input('Quantas partidas? '))
+for c in range(0, partidas):
+    gol = int(input(f'Quantos gols foram marcados na partida {c}: '))
     lista.append(gol)
-    gols += gol
-jogador['gols'] = gols
+    total += gol
+jogador['gols'] = lista[:]
+jogador['total'] = total
 print(jogador)
-
+print('=-' * 20)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+print('=-' * 20)
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
+for pos, v in enumerate(lista):
+    print(f'    => Na partida {pos}, fez {v} gols.')
+print(f'Foi um total de {total} gols.')
 
