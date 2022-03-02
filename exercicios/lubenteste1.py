64c7e7afe48ff16fd2b97dda0f27311eb125c524
Luan Bezerra <luan.silvapvh@gmail.com>
2022-03-02 12:16:32 -0400

    Curso finalizado com aproveitamentode 9/10 e criamos uma classe para solucionar o erro

---
+++ exercicios/lubenteste1.py
@@ -0,0 +1,28 @@
+num = [3, 6, 4, 1]
+for n1, n2 in enumerate(num):
+    print(n1 + n2)
+
+
+
+
+class Calculadora:
+    def __init__(self):
+        ...
+
+    def soma( numero_um, numero_dois):
+        calculo = numero_um + numero_dois
+        return calculo
+
+
+    def divisao(self, numero_um, numero_dois):
+        calculo = self.numero_um / self.numero_dois
+        return calculo
+
+
+
+xyz = Calculadora
+print(xyz.soma(1500,800))
+
+
+
+
