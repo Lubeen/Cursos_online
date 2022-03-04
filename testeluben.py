99d0f464623bb1357c12ad27bb40dbd9c6410042
Luan Bezerra <luan.silvapvh@gmail.com>
2022-03-04 11:46:13 -0400

    Fazendo uns desafios agora

--- testeluben.py
+++
@@ -1,9 +0,0 @@
-contador = 0
-nome = 'luan'
-while (contador <= 5):
-       print(contador)
-       contador   = contador + 1
-       if contador == 4:
-              nome = str(input('Digite seu nome'))
-print(nome)
-# editando
