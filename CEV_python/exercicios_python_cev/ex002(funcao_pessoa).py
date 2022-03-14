180b4c06dc1b3ebc0f87070347539ca44ea3b44b
Luan Bezerra <luan.silvapvh@gmail.com>
2022-03-14 11:23:53 -0400

    transformando os ex em funcoes

---
+++ CEV_python/exercicios_python_cev/ex002(funcao_pessoa).py
@@ -0,0 +1,8 @@
+def pessoa(nome, idade):
+     print(f'Seu nome é {nome.capitalize()} e sua idade é {idade} anos.')
+
+# Programa Principal
+nome = input('Qual é seu nome?')
+idade = int(input('Qual sua idade?'))
+pessoa(nome, idade)
+
--- CEV_python/exercicios_python_cev/ex002.py
+++
@@ -1,5 +0,0 @@
-nome = input('Qual é seu nome?')
-print('Olá, seja muito bem vindo, {}!'.format(nome))
-idade = input('Qual sua idade?')
-print('Sua idade é {}' .format(idade))
-
