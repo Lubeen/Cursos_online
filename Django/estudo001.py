1b126d986bf8ef425a8c501ad9ef8e3bee129f81
Luan Bezerra <luan.silvapvh@gmail.com>
2022-03-22 14:35:00 -0400

    pasta deletada por ter sido criada de forma errada, agora vamos criar um repositorio especial para ela

--- Django/estudo001.py
+++
@@ -1,8 +0,0 @@
-'''Primeira coisa é criar um ambite virtual,
-criamos o ambiente virtual no pycharm só que criamos com a versão errada e
-então vamos baixar o Anaconda, pois não conseguimos no pycharm,
-e infelizmente nao conseguimos acessar o anaconda, apesar de baixar não conseguimos executa-lo
-
-'''
-
-
