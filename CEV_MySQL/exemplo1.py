24a3c3316e4d699f7d39ce999800c8b3fbaa0e18
Luan Bezerra <luan.silvapvh@gmail.com>
2022-03-30 11:50:14 -0400

    anotações  CEV_MySQL

---
+++ CEV_MySQL/exemplo1.py
@@ -0,0 +1,49 @@
+"""MySQL comandos ou instruções
+
+DDL = Definição: alterar a definição a estrutura da base de dados, criar tabela etc
+
+DML = Manipulação: capaz de mover, criar, excluir, alterar as composicoes de dados etc
+
+DQL = Solicitações: select, qualquer solicitacao que precisar
+
+DCL = Controle: quais usuários acessam seu banco
+
+DTL = Transações: solicitações que pode ser feita a um banco de dados seguindo a DICA= 
+
+DURABILIDADE = O dado deve permanecer duravel
+
+ISOLAMENTO = se eu tiver duas ou mais solicitacoes precisam agir sem conflitos
+
+CONSISTÊNCIA = toda transacao deve levar um estado consistente a outro consistente
+
+ATOMICIDADE = toda transacao deve ser atomica ou toda acontece ou nada acontece, se nao ela dar um ctrl + z
+
+Banco de dados é composto de 4 partes:
+
+1:Base de dados
+
+2:SGBD = Sistema Gerenciador de banco de dados 
+
+3:Linguagem de Exploração
+
+4:Programas Adiconais
+
+MySQL comandos ou instruções
+
+DDL = Definição: alterar a definição a estrutura da base de dados, criar tabela etc
+
+DML = Manipulação: capaz de mover, criar, excluir, alterar as composicoes de dados etc
+
+DQL = Solicitações: select, qualquer solicitacao que precisar
+
+DCL = Controle: quais usuários acessam seu banco
+
+DTL = Transações: solicitações que pode ser feita a um banco de dados seguindo a DICA= 
+
+DURABILIDADE = O dado deve permanecer duravel
+
+ISOLAMENTO = se eu tiver duas ou mais solicitacoes precisam agir sem conflitos
+
+CONSISTÊNCIA = toda transacao deve levar um estado consistente a outro consistente
+
+ATOMICIDADE = toda transacao deve ser atomica ou toda acontece ou nada acontece, se nao ela dar um ctrl + z"""
