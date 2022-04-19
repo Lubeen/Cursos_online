92e9809a892834381357945feba5cbca33c5d038
Luan Bezerra <luan.silvapvh@gmail.com>
2022-04-19 14:00:53 -0400

    Atualizando a CEV_MySQL

---
+++ CEV_MySQL/exemplo6.py
@@ -0,0 +1,12 @@
+"""
+Gerenciando copias de segurança
+
+
+DUMP = criar um backup do servidor
+No MySQL workbench vá em server
+e clique no data export
+selecione export to Self-contained SQL
+
+E para importar é só fazer o mesmo procedimento
+porem selecionando o local do arquivo e o arquivo
+"""
