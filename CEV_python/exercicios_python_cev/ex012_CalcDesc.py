e7ee0db4243a305c03e3dd5c5cfca0ad27c8e181
Luan Bezerra <luan.silvapvh@gmail.com>
2022-04-21 10:40:39 -0400

    atualizando

--- CEV_python/exercicios_python_cev/ex012_CalcDesc.py
+++ CEV_python/exercicios_python_cev/ex012_CalcDesc.py
@@ -1,7 +1,8 @@
 def CalcDesc(preco_produto):
     desconto = preco_produto - (preco_produto / 100 * 5)
     return desconto
-
+    
+# Programa Principal
 preco_produto = float(input('Digite o valor do produto : R$'))
 valor_final = CalcDesc(preco_produto)
 print(f'O produto que custa R${preco_produto:.2f} reais está hoje na promoção de 5% por R${valor_final:.2f} reais')
